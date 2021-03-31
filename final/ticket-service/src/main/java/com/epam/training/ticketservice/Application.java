5ae1e8f1d19ffa74ea410a0e82488af709950709
Ferenc Barta <ferenc_barta@epam.com>
2021-04-01 01:58:55 +0200

    - Fixed pom.xml - Removed spring-specific code from main

--- final/ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
@@ -1,11 +1,6 @@
 package com.epam.training.ticketservice;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
     }
 }
