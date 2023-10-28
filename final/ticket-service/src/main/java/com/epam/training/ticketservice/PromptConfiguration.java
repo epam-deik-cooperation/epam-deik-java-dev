51194c273ef1c98d8a6cd9ce57b49752bb74bc2f
Negrea Adrián Krisztián <burnitdown01@gmail.com>
2023-10-28 18:43:58 +0200

    First changes

---
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/PromptConfiguration.java
@@ -0,0 +1,13 @@
+package com.epam.training.ticketservice;
+
+import org.jline.utils.AttributedString;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.shell.jline.PromptProvider;
+
+@Configuration
+public class PromptConfiguration implements PromptProvider {
+    @Override
+    public AttributedString getPrompt() {
+        return new AttributedString("Ticket service>");
+    }
+}
