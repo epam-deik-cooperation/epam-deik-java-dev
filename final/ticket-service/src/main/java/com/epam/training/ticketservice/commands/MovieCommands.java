743715ffc08152e1156b99746c6532f892444a77
Negrea Adrián Krisztián <burnitdown01@gmail.com>
2023-10-31 10:23:24 +0100

    Movie updater command implemented

--- final/ticket-service/src/main/java/com/epam/training/ticketservice/commands/MovieCommands.java
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/commands/MovieCommands.java
@@ -1,6 +1,7 @@
 package com.epam.training.ticketservice.commands;
 
 import com.epam.training.ticketservice.logsystem.LogSystem;
+import com.epam.training.ticketservice.models.moviemodel.Movie;
 import com.epam.training.ticketservice.models.moviemodel.Movies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.Availability;
@@ -23,6 +24,19 @@ public class MovieCommands {
     @ShellMethod(key = "create movie", value = "Creates a movie")
     public void createMovie(String movieName, String genre, int length){
         movies.append(movieName, genre, length);
+    }
+
+    @ShellMethodAvailability("isAvailable")
+    @ShellMethod(key = "update movie", value = "Updates an existing movie")
+    public String movieUpdater(String movieName, String genre, int length){
+        for (Movie movie : movies.getMovies()) {
+            if (movie.getName().equals(movieName)){
+                movie.setGenre(genre);
+                movie.setLengthInTime(length);
+                return "Movie successfully updated.";
+            }
+        }
+        return "Movie was not found.";
     }
 
     @ShellMethod(key = "list movies", value = "Lists all created movies")
